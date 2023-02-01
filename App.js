776d629cc7d95861901d9470ed41c8b0db6b6ee9
Scream10 <nicolasdiot10@gmail.com>
2023-02-01 16:51:21 +0100

    feat - show maps with react-native-maps

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
-export default function App() {
+const TOKYO = {
+  latitude: 35.6762,
+  longitude: 139.6503,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+};
+
+const CHIBA = {
+  latitude: 35.6074,
+  longitude: 140.1065,
+  latitudeDelta: 0.01,
+  longitudeDelta: 0.01,
+};
+
+const App = () => {
+  const [region, setRegion] = useState(TOKYO);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={TOKYO}
+        onRegionChangeComplete={(region) => setRegion(region)}
+        showsCompass
+        followsUserLocation
+      >
+        <Marker coordinate={TOKYO} />
+        <Marker coordinate={CHIBA} />
+      </MapView>
+      <View style={styles.containerText}>
+        <Text style={styles.text}>Current latitude: {region.latitude}</Text>
+        <Text style={styles.text}>Current longitude: {region.longitude}</Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    ...StyleSheet.absoluteFillObject,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  text: {
+    marginBottom: 6,
+  },
+  containerText: {
+    backgroundColor: "white",
+    borderRadius: 12,
+    paddingTop: 12,
+    padding: 6,
+    marginBottom: 40,
   },
 });
+
+export default App;
